ab4d251490624a39c97dea22d2de8ee97413bd0a
Melissa Mojica <melissa_mojica23@hotmail.com>
2022-01-28 22:09:40 -0500

    gavity and jumping

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    
+    private Rigidbody2D rb;
+    
+    public bool facingRight = true;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate(){
+        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput*speed, rb.velocity.y);
+        
+        if(facingRight == false && moveInput > 0){
+            Flip();
+        } else if (facingRight == true && moveInput <0){
+            Flip();
+        }
+    }
+    
+    void Update(){
+        
+        /*if(Input.GetKeyDown(KeyCode.W)){
+            rb.velocity = Vector2.up * jumpForce;
+        }*/
+    }
+    
+    void Flip(){
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+    
+}
