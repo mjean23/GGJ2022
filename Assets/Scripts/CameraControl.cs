62bc6a5d3835c8a1b6a3b6f5fb48149d51fdbef3
Melissa Mojica <melissa_mojica23@hotmail.com>
2022-01-28 22:48:17 -0500

    fixed camer
    Camera now follows player without flipping on gravity switch

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    public Transform player;
+
+    void Update () {
+        transform.position = player.transform.position + new Vector3(0, 1, -5);
+    }
+}
